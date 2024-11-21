645a667f9cdc8b706523d16920c68b540e43bcc9
Nick Savva <nsavva38@gmail.com>
2024-11-20 21:27:15 -0500

    everything seems to be working. must fix functionality for 'Get user owned playlists

--- routes/tracks.js
+++ routes/tracks.js
@@ -4,6 +4,8 @@ module.exports = router;
 
 const prisma = require("../prisma");
 
+const { authenticate } = require("./auth");
+
 router.get("/", async (req, res, next) => {
   try {
     const tracks = await prisma.track.findMany();
@@ -27,6 +29,21 @@ router.get("/:id", async (req, res, next) => {
         status: 404,
         message: `Track with id ${id} does not exist`
       })
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get("/:id", authenticate, async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const track = await prisma.track.findUnique({ where: { id: +id}});
+    if (track) {
+      res.json(track);
+    } else {
+      next({ status: 403, message: `You do not own this track`});
     }
   } catch (e) {
     next(e);
